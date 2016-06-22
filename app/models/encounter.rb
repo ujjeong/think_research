35eba8f5954c46f321b705c943b8964dc06c86fc
Eugene Jeong <eugene@knowroaming.com>
2016-06-21 20:36:06 -0400

    date comparison

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -1,9 +1,13 @@
 class Encounter < ActiveRecord::Base
  	belongs_to :patient
  	validates :visit_number, :admitted_at, :discharged_at, presence: true
+	validates :bed, :room, numericality: { only_integer: true }
  	validate :checkAdmittBeforeDischarge
 
+ 	# Check date comparison with admitted_at and discharged_at
  	def checkAdmittBeforeDischarge
-	    errors.add(:admitted_at, "admitted time must be before discharged time") unless admitted_at > discharged_at
+ 		if admitted_at and discharged_at and admitted_at > discharged_at
+	    	errors.add(:admitted_at, "must be before discharged time")
+	    end
 	end 
 end
