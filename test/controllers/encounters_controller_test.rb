8b4df63ee54f0688af1e1715ab306300528c00af
Eugene Jeong <eugene@knowroaming.com>
2016-06-22 00:56:57 -0400

    bug fixes

--- test/controllers/encounters_controller_test.rb
+++ test/controllers/encounters_controller_test.rb
@@ -1,7 +1,17 @@
-require 'test_helper'
+require '../test_helper'
 
 class EncountersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  	test "should not save encounters without parameters" do
+  		encounters = Encounter.new
+  		assert_not encounters.save
+  	end
+
+  	test "should save encounters with missing optional parameters" do
+  		encounters = Encounter.new(admitted_at: "1992-05-27", discharged_at: "1992-06-20", visit_number: "MN002")
+  		assert encounters.save
+  	end
+
+  	test "encounters show routing action" do
+		assert_routing '/patients/1/encounters/1', controller: "encounters", action: "show", patient_id: "1", id: "1"
+	end
 end
--- test/controllers/patients_controller_test.rb
+++ test/controllers/patients_controller_test.rb
@@ -1,7 +1,24 @@
-require 'test_helper'
+require '../test_helper'
 
 class PatientsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "should be able to get index" do
+		get :index
+		assert_template :index
+		assert_response 200
+	end
+
+	test "should not save patient without any parameters" do
+		@patients = Patient.new
+		assert_not @patients.save
+	end
+
+	test "should save patient with missing optional parameter" do
+		@patients = Patient.create(first_name: "eugene", middle_name: "jin", mrn: "mrn200")
+		assert @patients.save
+	end
+
+	test "routing action" do
+		assert_routing '/patients', controller: "patients", action: "index"
+		assert_routing '/patients/1', controller: "patients", action: "show", id: "1"
+	end
 end
